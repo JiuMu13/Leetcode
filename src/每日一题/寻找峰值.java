1977287e586366eb70c909afcb9c900576f457b2
JiuMu <2446094496@qq.com>
2023-12-18 12:09:36 +0800

    12月18日交 每日一题

---
+++ src/每日一题/寻找峰值.java
@@ -0,0 +1,26 @@
+package 每日一题;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+//二分
+public class 寻找峰值 {
+    public int findPeakElement(int[] nums) {
+        int l=0,r=nums.length-1;
+        while (l<r){
+            int mid=(l+r)/2;
+            if (nums[mid]>nums[mid+1]){
+                r=mid;
+            }else {
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        寻找峰值 t=new 寻找峰值();
+        int[] n={1,2,3,4,5};
+        System.out.println(t.findPeakElement(n));
+    }
+}
